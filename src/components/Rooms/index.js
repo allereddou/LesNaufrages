e4cd0e83132fedbb936cb4c83ee219aa98e5676e
Olivier Gamache <ogamache@nexapp.ca>
2019-11-23 12:26:22 -0500

    rooms

--- src/components/Rooms/index.js
+++ src/components/Rooms/index.js
@@ -2,14 +2,25 @@ import React from "react";
 import { compose, fromRenderProps } from "recompose";
 
 class Rooms extends React.Component {
+  state = {
+    room: ''
+  }
+
+  handleRoomChange = (event) => {
+    this.setState({ room: event.target.value })
+  }
+
+  joinRoom = () => {
+
+  }
+
   render() {
-    const { rooms } = this.props
     return (
       <div>
-        {rooms.map(a => <div>{room.name}</div>)}
-        <div>
-
-        </div>
+        <input type="text" value={this.state.room} onChange={this.handleRoomChange} />
+        <button onClick={this.joinRoom}>
+          Join Room
+        </button>
       </div>
     )
   }
